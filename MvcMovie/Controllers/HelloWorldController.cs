a62449f6e3038dda947f0852a891e104bf71a4d1
KisuraWSP <kisura2003@gmail.com>
2026-04-20 16:20:15 +0530

    Add project files.

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+
+    }
+}
